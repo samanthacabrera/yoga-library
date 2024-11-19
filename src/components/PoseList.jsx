3864d3633f8bddf7b5374397cacc94f6c607ef88
Samantha Cabrera <samantha.n.cabrera@gmail.com>
2024-11-18 17:00:27 -0700

    added alphabetical sort to PoseList

--- src/components/PoseList.jsx
+++ src/components/PoseList.jsx
@@ -4,21 +4,35 @@ import posesData from '../data/poses.json';
 
 const PoseList = () => {
   const [poses, setPoses] = useState([]);
+  const [isSorted, setIsSorted] = useState(false); 
 
   useEffect(() => {
     setPoses(posesData);
   }, []);
 
+  const toggleSort = () => {
+    setIsSorted(!isSorted);
+    setPoses((prevPoses) =>
+      [...prevPoses].sort((a, b) =>
+        isSorted ? a.id - b.id : a.name.localeCompare(b.name)
+      )
+    );
+  };
+
   return (
     <div className="flex flex-col flex-col-grow min-w-44 lg:min-w-64 overflow-y-scroll space-y-2 p-4 bg-gray-100">
+      <button
+        onClick={toggleSort}
+        className="self-start opacity-50 hover:opacity-60"
+      >
+        sort by: {isSorted ? 'more → less common' : 'A → Z'}
+      </button>
       {poses.length === 0 ? (
         <p>No poses available.</p>
       ) : (
         poses.map((pose) => (
           <div key={pose.id} className="hover:-translate-y-1 transition duration-200">
-            <Link to={`/pose/${pose.id}`}>
-              {pose.name}
-            </Link>
+            <Link to={`/pose/${pose.id}`}>{pose.name}</Link>
           </div>
         ))
       )}
