eac0dbf1a1c05717ccd82961ac9dbb324b290eed
Samantha Cabrera <samantha.n.cabrera@gmail.com>
2024-12-19 14:50:36 -0700

    add email feedback submission button to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,12 +4,19 @@ import { Link } from "react-router-dom";
 const Footer = () => {
     return (
         <div className="flex justify-center opacity-50 py-1">
-            <p>made by <a href="https://github.com/samanthacabrera" target="_blank" className="hover:italic hover:underline">sam cabrera</a></p>
-            <p className="absolute left-2">
+            <div className="absolute left-2">
                 <Link to="/privacy-policy" className="hover:italic hover:underline">
                     Privacy Policy
                 </Link>
-            </p>
+            </div>
+
+            <p>Made by <a href="https://github.com/samanthacabrera" target="_blank" className="hover:italic hover:underline">sam cabrera</a></p>
+
+            <div className="absolute right-2">
+                <form action="mailto:samantha.n.cabrera@gmail.com" method="POST" enctype="text/plain">
+                    <button type="submit" >Feedback</button>
+                </form>
+            </div>
         </div>
     )
 }
