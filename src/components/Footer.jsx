7c4899763ee975320acdf5a6f1bf4fa141072755
Samantha Cabrera <samantha.n.cabrera@gmail.com>
2025-02-14 11:08:16 -0700

    update pose list to one column, styled footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <div className="bg-moss bg-opacity-90">
             <PoseList />
 
-            <footer className="p-6 mx-4 text-sm">
+            <footer className="p-6 mx-4 text-sm text-white">
                 <div className="flex flex-col-reverse md:flex-row justify-between items-center md:items-end space-y-6 md:space-y-0">
                     
                     <div className="text-center md:text-left">
@@ -18,14 +18,14 @@ const Footer = () => {
                             rel="noopener noreferrer" 
                             className="italic"
                         >
-                            made by <span className="hover:text-white duration-300 ease-in-out">Sam Cabrera</span>
+                            made by Sam Cabrera
                         </a>
                     </div>
 
                     <div className="flex flex-col md:flex-row p-2 space-y-2 md:space-y-0 md:space-x-8 text-center md:text-right">
-                        <Link to="/" className="hover:text-white duration-300 ease-in-out">Home</Link>
-                        <Link to="/newsletter" className="hover:text-white duration-300 ease-in-out">Newsletter</Link>
-                        <Link to="/privacy-policy" className="hover:text-white duration-300 ease-in-out">Privacy</Link>
+                        <Link to="/" className="hover:opacity-80 duration-300 ease-in-out">Home</Link>
+                        <Link to="/newsletter" className="hover:opacity-80 duration-300 ease-in-out">Newsletter</Link>
+                        <Link to="/privacy-policy" className="hover:opacity-80 duration-300 ease-in-out">Privacy</Link>
                     </div>
                 </div>
             </footer>
--- src/components/PoseList.jsx
+++ src/components/PoseList.jsx
@@ -4,22 +4,30 @@ import posesData from '../data/poses.json';
 
 const PoseList = () => {
   return (
-    <div className="mt-40 p-6 md:p-12">
+    <div className="mt-40 py-8 px-8 md:px-20">
       <Link
         to="/poses"
-        className="text-white text-lg lg:text-2xl tracking-wide font-medium hover:border-b-2">
+        className="text-white text-2xl lg:text-4xl tracking-tight hover:opacity-70 transition duration-300 block border-b border-white py-2 my-12">
         Yoga Pose Directory
       </Link>
 
       {posesData.length === 0 ? (
-        <p className="text-center">No poses available.</p>
+        <p className="text-center text-gray-500 mt-24">No poses available.</p>
       ) : (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 py-6 md:py-12">
+        <div className="grid grid-cols-1 gap-2 text-white">
           {posesData.map((pose) => (
-            <div key={pose.id} className="p-4 bg-white border-2 rounded-2xl border-moss hover:bg-moss hover:bg-opacity-50 hover:text-white hover:border-white transition duration-300">
-              <Link to={`/poses/${pose.name}`} className="block text-sm font-medium">
+            <div 
+              key={pose.id} 
+              className="relative group p-4 border border-white rounded-lg hover:border-black transition duration-500"
+            >
+              <Link 
+                to={`/poses/${pose.name}`} 
+                className="block text-xs tracking-wide text-left group-hover:text-black transition-colors duration-500"
+              >
                 {pose.name}
               </Link>
+              <span className="absolute bottom-4 right-4 text-sm text-gray-400 group-hover:text-black transition duration-500">→</span>
+              <div className="absolute top-0 left-0 w-0 h-full bg-black opacity-5 group-hover:w-full transition-all duration-500"></div>
             </div>
           ))}
         </div>
